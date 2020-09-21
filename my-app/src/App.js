db7dca3b1a12ea9b1235008d3f579600b3084c7c
Yurii <pshetskiy@gmail.com>
2020-09-21 17:14:36 +0300

    fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
-import Header from "./components/header";
+import Header from "./components/Header/Header";
 
 const useStyles = makeStyles({
     root: {
@@ -9,14 +9,12 @@ const useStyles = makeStyles({
     }
 })
 
-function App() {
+const App = () => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
             <Header />
         </div>
-
-
     );
 }
 
